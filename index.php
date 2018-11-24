f5f9fd4175810dfea065703778f2385f85d42bdb
Rulo GM <RutilaXD@users.noreply.github.com>
2018-11-24 11:03:40 -0600

    Página de inicio
    Falta agregar fondo de pantalla para dar presentacion así como el título para que se indique es la página de inicio

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Taxkali - Inicio</title>
+	<link rel="stylesheet" type="text/css" href="./css/style_menu.css">
+</head>
+
+<body>
+	<div id="productos">
+		<div class="row">
+			<div class="prod" title="Tortilla normal" style="background-image: url('./img/tortillas.jpg'); background-size: cover;" onclick="window.location.href='./pedidos/RealizarPedidos.php';"></div>
+			<div class="prod" title="Tortilla pequeña" style="background-image: url('./img/pequet.jpg'); background-size: cover;" onclick="window.location.href='./pedidos/PedidoPequena.php';"></div>
+			<div class="prod" title="Tlacoyos" style="background-image: url('./img/tlacoyos'); background-size: cover;" onclick="window.location.href='./pedidos/PedidoTlacoyos.php';"> </div>
+		</div>
+		<div class="row">
+			<div class="prod" title="Masa" style="background-image: url('./img/masa.JPG'); background-size: cover;" onclick="window.location.href='./pedidos/PedidoMasa.php';"></div>
+			<div class="prod" title="Sopes" style="background-image: url('./img/sopes.JPG'); background-size: cover;" onclick="window.location.href='./pedidos/PedidoSopes.php';"></div>
+			<div class="prod" title="Totopos" style="background-image: url('totopos.png'); background-size: cover;" onclick="window.location.href='./pedidos/PedidoToto.php';"></div>
+		</div>
+		<div class="row">
+			<div class="prod" title="Salsas" style="background-image: url('./img/salsas.jpg'); background-size: cover;" onclick="window.location.href='PedidoSalsa.php';"></div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
